f0c2f8a117ab59a8e0a6177b417d2cfca4c09438
Ngoc Quang <bme.nnquang@gmail.com>
2014-04-29 10:33:55 +0700

    Visualizing the given data -> missing data (unlabeled data and test)

---
+++ VisualizeRawData.m
@@ -0,0 +1,39 @@
+% Initializing
+close all
+factor = 0;
+range = 12;
+row = 3;
+col = 4;
+
+% Visualizing test data
+cd ../Test
+figure;
+set(gcf, 'name', 'TEST DATA')
+for i = (1+factor):(range+factor)
+    filename = strcat('image_', num2str(i));
+    im = dicomread(filename);
+    subplot(row, col, i);
+    imshow(im,[]);
+end
+
+% Visualizing train data (unlabeled)
+cd ../Train/PATIENT_DICOM
+figure;
+set(gcf, 'name', 'TEST DATA')
+for i = (1+factor):(range+factor)
+    filename = strcat('image_', num2str(i));
+    im = dicomread(filename);
+    subplot(row, col, i);
+    imshow(im,[]);
+end
+
+% Visualizing train data (labeled)
+cd ../MASKS_DICOM/bone
+figure;
+set(gcf, 'name', 'TEST DATA')
+for i = (1+factor):(range+factor)
+    filename = strcat('image_', num2str(i));
+    im = dicomread(filename);
+    subplot(row, col, i);
+    imshow(im,[]);
+end
